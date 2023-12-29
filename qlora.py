ca252f792a9f87da021bba75a5f64387d615057c
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2023-12-29 14:53:19 +0100

    checkin

---
+++ qlora.py
@@ -0,0 +1,233 @@
+import torch
+import os
+import wandb
+import uuid
+import json
+import bitsandbytes as bnb
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, BitsAndBytesConfig, TrainerCallback
+from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
+from accelerate import Accelerator
+from datasets import load_dataset, DatasetDict, Dataset,load_from_disk
+
+## TODO
+# - change config to add eos/pad_token - how? to model? to gen. config?
+
+def read_json(file_path):
+    f = open(file_path)
+    data = json.load(f)
+    f.close()
+    return data
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    # if args.bits == 4: 
+    trainable_params /= 2
+    print(
+        f"trainable params: {trainable_params} || "
+        f"all params: {all_param} || "
+        f"trainable: {100 * trainable_params / all_param}"
+    )
+
+
+def find_all_linear_names(model):
+    cls = bnb.nn.Linear4bit # if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if 'lm_head' in lora_module_names: # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+accelerator = Accelerator()
+
+run_id = str(uuid.uuid4())
+bs=1        # batch size
+bs_eval=16        # batch size
+ga_steps=16  # gradient acc. steps
+epochs=40
+modelpath="models/phi-2"
+lr=0.00004
+max_length=2048
+dataset_name="synthetic_riddles_step-4_ACC.json"
+output_dir=f"out-{run_id}"
+
+lora_config = LoraConfig(
+    r=32, 
+    lora_alpha=32, 
+    # target_modules=find_all_linear_names(model),
+    target_modules = ['Wqkv','out_proj'],
+    lora_dropout=0.1, 
+    bias="none", 
+    modules_to_save = ["lm_head", "embed_tokens"],
+    task_type="CAUSAL_LM"
+)
+
+# Load model
+model = AutoModelForCausalLM.from_pretrained(
+    modelpath,    
+    device_map={"": accelerator.process_index},
+    quantization_config=BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_quant_type="nf4",
+    ),
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+)
+
+# Load Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(modelpath, use_fast=False)    # fast tokenizer sometimes ignores the added tokens
+
+# Add tokens <|im_start|> and <|im_end|>, latter is special eos token, 
+tokenizer.add_tokens(["<|im_start|>", "<PAD>"])
+tokenizer.pad_token = "<PAD>"
+tokenizer.add_special_tokens(dict(eos_token="<|im_end|>"))
+model.resize_token_embeddings(
+    new_num_tokens=len(tokenizer),
+    pad_to_multiple_of=64)   # phi2 default is 64, see configuration_phi.py
+model.config.eos_token_id = tokenizer.eos_token_id
+
+# Add adapters to model
+model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=False)
+model = get_peft_model(model, lora_config)
+model.gradient_checkpointing=True
+model.config.use_cache = False
+
+# Load dataset
+synthetic_riddles=read_json(dataset_name)
+dataset = Dataset.from_list(synthetic_riddles)
+dataset = dataset.train_test_split(test_size=0.1)
+
+# Format (chatML) and tokenize dataset
+templates=[
+    "<|im_start|>assistant\n{msg}<|im_end|>",
+    "<|im_start|>user\n{msg}<|im_end|>"
+]
+IGNORE_INDEX=-100
+
+def tokenize(input):
+    input_ids, attention_mask, labels = [], [], []
+
+    for i,msg in enumerate(input["messages"]):
+        isHuman = i%2==0
+        msg_chatml=templates[isHuman].format(msg=msg)
+        msg_tokenized=tokenizer(msg_chatml, truncation=False, add_special_tokens=False)
+    
+        input_ids+=msg_tokenized["input_ids"]
+        attention_mask+=msg_tokenized["attention_mask"]
+        labels+=[IGNORE_INDEX]*len(msg_tokenized["input_ids"]) if isHuman else msg_tokenized["input_ids"]
+
+    return {
+        "input_ids": input_ids[:max_length],
+        "attention_mask": attention_mask[:max_length],
+        "labels": labels[:max_length],
+    }
+
+dataset_tokenized = dataset.map(
+    tokenize, 
+    batched=False, 
+    num_proc=os.cpu_count()//accelerator.num_processes,    # multithreaded
+    remove_columns=dataset["train"].column_names  # don't need this anymore, we have tokens from here on
+)
+
+# collate function - to transform list of dictionaries [ {input_ids: [123, ..]}, {.. ] to single batch dictionary { input_ids: [..], labels: [..], attention_mask: [..] }
+def collate(elements):
+    tokens=[e["input_ids"] for e in elements]
+    tokens_maxlen=max([len(t) for t in tokens])
+
+    for i,sample in enumerate(elements):
+        input_ids=sample["input_ids"]
+        labels=sample["labels"]
+        attention_mask=sample["attention_mask"]
+
+        pad_len=tokens_maxlen-len(input_ids)
+
+        input_ids.extend( pad_len * [tokenizer.pad_token_id] )   
+        labels.extend( pad_len * [IGNORE_INDEX] )    
+        attention_mask.extend( pad_len * [0] ) 
+
+    batch={
+        "input_ids": torch.tensor( [e["input_ids"] for e in elements] ),
+        "labels": torch.tensor( [e["labels"] for e in elements] ),
+        "attention_mask": torch.tensor( [e["attention_mask"] for e in elements] ),
+    }
+
+    # breakpoint()
+
+    return batch
+
+steps_per_epoch=len(dataset_tokenized["train"])//(accelerator.num_processes*bs*ga_steps)
+
+args = TrainingArguments(
+    output_dir=output_dir,
+    per_device_train_batch_size=bs,
+    per_device_eval_batch_size=bs_eval,
+    evaluation_strategy="steps",
+    logging_steps=1,
+    eval_steps=steps_per_epoch//2,
+    save_steps=steps_per_epoch,
+    gradient_accumulation_steps=ga_steps,
+    num_train_epochs=epochs,
+    lr_scheduler_type="constant",
+    optim="paged_adamw_32bit",      # val_loss will go nan with paged_adamw_8bit
+    learning_rate=lr,
+    group_by_length=False,
+    bf16=True,        
+    ddp_find_unused_parameters=False,
+)
+
+print_trainable_parameters(model)
+dtypes = {}
+for _, p in model.named_parameters():
+    dtype = p.dtype
+    if dtype not in dtypes: dtypes[dtype] = 0
+    dtypes[dtype] += p.numel()
+total = 0
+for k, v in dtypes.items(): total+= v
+for k, v in dtypes.items():
+    print(k, v, v/total)
+
+
+trainer = Trainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=args,
+    data_collator=collate,
+    train_dataset=dataset_tokenized["train"],
+    eval_dataset=dataset_tokenized["test"],
+)
+
+# wandb.init(mode="disabled")
+if accelerator.is_main_process:
+    run = wandb.init(
+        project="phi2",
+        name=modelpath.split("/")[1]+"_"+dataset_name+f"_bs-{bs}_LR-{lr}_maxlen-{max_length}_{run_id}",
+        config={
+            "model_name": modelpath,
+            "run_id": run_id,
+            "dataset": dataset_name,
+            "output_dir": output_dir,
+            "lr": lr,
+            "max_length": max_length,
+            "train_batch_size": bs,
+            "validation_batch_size": bs,
+            "ga_steps": ga_steps,
+            "lora_config": lora_config, 
+            "training_args": args,
+            "GPUs": accelerator.num_processes,
+        }
+    )
+
+trainer.train()
+
